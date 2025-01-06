9186dbd26f7a5ac6a67ff3ef709239f97726909b
Dmitrii Losunov <dmitrij.dmitriy32@yandex.ru>
2025-01-05 21:53:48 -0800

    Добавлены тесты с параметризацией и Allure-декораторами

---
+++ tests/test_bun.py
@@ -0,0 +1,16 @@
+import allure
+import pytest
+from Stellar_Burgers.bun import Bun
+
+@allure.feature('Проверка создания булочки с разными параметрами')
+class TestBun:
+    @allure.title('Проверка инициализации булочки с параметрами: {name}, {price}')
+    @pytest.mark.parametrize("name, price", [
+        ("black bun", 100),
+        ("white bun", 200),
+        ("red bun", 300),
+    ])
+    def test_bun_initialization(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_name() == name, f"Имя булочки должно быть {name}"
+        assert bun.get_price() == price, f"Цена булочки должна быть {price}"
